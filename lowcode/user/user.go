fae42a4892adfdb2dc78902ee37bb6a579092481
went <wentevill@gmail.com>
2022-05-05 11:45:54 +0800

    demo of get user profile

--- lowcode/user/user.go
+++ lowcode/user/user.go
@@ -2,9 +2,18 @@ package user
 
 import "context"
 
-type GetProfileReq struct{}
+type GetProfileReq struct {
+	ID string `json:"id,omitempty"`
+}
 
-type GetProfileResp struct{}
+type GetProfileResp struct {
+	Code int64  `json:"code,omitempty"`
+	Msg  string `json:"msg,omitempty"`
+	Data struct {
+		Name  string `json:"name,omitempty"`
+		Email string `json:"email,omitempty"`
+	} `json:"data,omitempty"`
+}
 
 type User interface {
 	// GetProfile
